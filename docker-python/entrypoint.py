d71ab38b18902d60b502f09a63296dc83b0b2dfd
Chandolkar001 <chandolkarshreyas001@gmail.com>
2024-01-06 15:57:31 +0530

    test for path

--- docker-python/entrypoint.py
+++ docker-python/entrypoint.py
@@ -1,11 +1,25 @@
 import sys
 from app import HelloWorld
 import subprocess
+import os
+
+def get_file_mapping():
+    root_dir = "."
+    file_map = dict()
+
+    for dir_, _, files in os.walk(root_dir):
+        for file_name in files:
+            path = os.path.join(dir_, file_name)
+            mapping_path = os.path.relpath(path, root_dir)
+            file_map[path] = mapping_path
+    return file_map
 
 if __name__ == '__main__':
     
     result = subprocess.run(['ls', '-l'], stdout=subprocess.PIPE)
     print(result.stdout.decode())
+    print(get_file_mapping())
+
 
     if sys.argv:
         hw = HelloWorld(sys.argv[1])
