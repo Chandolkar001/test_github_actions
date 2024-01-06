8c023c215172e165835abc094823c4965e704337
Chandolkar001 <chandolkarshreyas001@gmail.com>
2024-01-06 12:37:25 +0530

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
 
-set -e
-
-while getopts "a:c:" o; do
-    case "${o}" in
-    a) 
-        name="${OPTARG}"
-        ;;
-    c)
-        custom_option="${OPTARG}"
-        ;;
-    *)
-        echo "Usage: $0 -a <name> -c <custom_option>"
-        exit 1
-        ;;
-    esac
-done
-
-# Additional processing if needed
-echo "Name: $name"
-echo "Custom Option: $custom_option"
-
-# Call Python script with the provided name
-python /app/entrypoint.py "$name"
+echo "Name: $1"
+python /app/entrypoint.py "$1"
