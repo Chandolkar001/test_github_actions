d8651ff4d3724b9aa308d9375f657383d351d90f
Chandolkar001 <chandolkarshreyas001@gmail.com>
2024-01-06 15:41:02 +0530

    test for files

--- docker-python/entrypoint.py
+++ docker-python/entrypoint.py
@@ -1,7 +1,12 @@
 import sys
 from app import HelloWorld
+import subprocess
 
 if __name__ == '__main__':
+    
+    result = subprocess.run(['ls', '-l'], stdout=subprocess.PIPE)
+    print(result.stdout.decode())
+
     if sys.argv:
         hw = HelloWorld(sys.argv[1])
         print(hw.greet(12))
