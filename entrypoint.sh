fc1eb977f5a56e08032976e6d7589f5c93c071ce
Chandolkar001 <chandolkarshreyas001@gmail.com>
2024-01-09 19:08:56 +0530

    check content of github

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,10 @@
 echo "Start secret scan"
 cd ..
 cd ..
+echo "search for event.json"
 find / -type f -name "event.json"
+echo "what's in /github"
+ls github/
 cd app/
 python secretscan_entry.py "$1" "$2" "$3" "$4"
 
