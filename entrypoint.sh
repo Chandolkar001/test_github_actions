08580824e7258c624a5d83d7b4a5a1681190e5bf
Chandolkar001 <chandolkarshreyas001@gmail.com>
2024-01-09 19:04:39 +0530

    search for event.json

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,7 @@
 echo "Start secret scan"
 cd ..
 cd ..
-ls github/workspace/
-find ~/ -type f -name "event.json"
+find / -type f -name "event.json"
 cd app/
 python secretscan_entry.py "$1" "$2" "$3" "$4"
 
