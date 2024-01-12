df3d95cf7bbb155f9fa58badc3cac0484ef6c95d
Chandolkar001 <chandolkarshreyas001@gmail.com>
2024-01-13 00:15:06 +0530

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,30 +5,30 @@ echo "Reading Parameters"
 while getopts ":a:b:c:d:e:" opt; do
   case $opt in
     a)
-      SCAN_TYPE="$OPTARG"
+      export SCAN_TYPE="$OPTARG"
       ;;
     b)
-      FILE_EXCLUSIONS="$OPTARG"
+      export FILE_EXCLUSIONS="$OPTARG"
       ;;
     c)
-      REGEX_EXCLUSIONS="$OPTARG"
+      export REGEX_EXCLUSIONS="$OPTARG"
       ;;
     d)
-      HASH_EXCLUSIONS="$OPTARG"
+      export HASH_EXCLUSIONS="$OPTARG"
       ;;
     e)
-      CUSTOM_REGEX="$OPTARG"
+      export CUSTOM_REGEX="$OPTARG"
       ;;
   esac
 done
 
-CLIENT_REPO=$GITHUB_WORKSPACE
-
-echo "$(pwd)"
-cd ../..
-cd app/
-echo "$(pwd)"
-
+export CLIENT_REPO=$GITHUB_WORKSPACE
+# export CLIENT_REPO=$(pwd)
+# echo "$(pwd)"
+# cd ../..
+# cd app/
+# echo "$(pwd)"
+cd /app
 echo "Start scan"
 python secretscan_entry.py --type "$SCAN_TYPE" --file "$FILE_EXCLUSIONS" --regex "$REGEX_EXCLUSIONS" --hash "$HASH_EXCLUSIONS" --cregex "$CUSTOM_REGEX" --code "$CLIENT_REPO"
 
