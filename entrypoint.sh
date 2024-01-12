b1c6ddabe0529203890f065b28ab7b27ab3eb52b
Chandolkar001 <chandolkarshreyas001@gmail.com>
2024-01-12 21:10:03 +0530

    restruct test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,34 @@
 #!/bin/sh -l
 
-echo "Start scan"
-client_repo=$GITHUB_WORKSPACE
+echo "Reading Parameters"
+
+while getopts ":a:b:c:d:e:" opt; do
+  case $opt in
+    a)
+      SCAN_TYPE="$OPTARG"
+      ;;
+    b)
+      FILE_EXCLUSIONS="$OPTARG"
+      ;;
+    c)
+      REGEX_EXCLUSIONS="$OPTARG"
+      ;;
+    d)
+      HASH_EXCLUSIONS="$OPTARG"
+      ;;
+    e)
+      CUSTOM_REGEX="$OPTARG"
+      ;;
+  esac
+done
+
+CLIENT_REPO=$GITHUB_WORKSPACE
+
+echo "$(pwd)"
 cd ../..
 cd app/
-echo "$client_repo"
-python secretscan_entry.py "$1" "$2" "$3" "$4"
+echo "$(pwd)"
+
+echo "Start scan"
+python secretscan_entry.py --type "$SCAN_TYPE" --file "$FILE_EXCLUSIONS" --regex "$REGEX_EXCLUSIONS" --hash "$HASH_EXCLUSIONS" --cregex "$CUSTOM_REGEX" --code "$CLIENT_REPO"
 
