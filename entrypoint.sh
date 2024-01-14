82897de6db86323404d330ea769db70e4571a2ed
Chandolkar001 <chandolkarshreyas001@gmail.com>
2024-01-14 12:05:06 +0530

    test for secret scan

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,34 +1,46 @@
+# #!/bin/sh -l
+
+# echo "Reading Parameters"
+
+# while getopts ":a:b:c:d:e:" opt; do
+#   case $opt in
+#     a)
+#       export SCAN_TYPE="$OPTARG"
+#       ;;
+#     b)
+#       export FILE_EXCLUSIONS="$OPTARG"
+#       ;;
+#     c)
+#       export REGEX_EXCLUSIONS="$OPTARG"
+#       ;;
+#     d)
+#       export HASH_EXCLUSIONS="$OPTARG"
+#       ;;
+#     e)
+#       export CUSTOM_REGEX="$OPTARG"
+#       ;;
+#   esac
+# done
+
+# export CLIENT_REPO=$GITHUB_WORKSPACE
+# cd /app
+# echo "Start scan"
+# python action_entrypoint.py --type "$SCAN_TYPE" --file "$FILE_EXCLUSIONS" --regex "$REGEX_EXCLUSIONS" --hash "$HASH_EXCLUSIONS" --cregex "$CUSTOM_REGEX" --code "$CLIENT_REPO"
+
 #!/bin/sh -l
 
 echo "Reading Parameters"
 
-while getopts ":a:b:c:d:e:" opt; do
+while getopts ":a:" opt; do
   case $opt in
     a)
       export SCAN_TYPE="$OPTARG"
-      ;;
-    b)
-      export FILE_EXCLUSIONS="$OPTARG"
-      ;;
-    c)
-      export REGEX_EXCLUSIONS="$OPTARG"
-      ;;
-    d)
-      export HASH_EXCLUSIONS="$OPTARG"
-      ;;
-    e)
-      export CUSTOM_REGEX="$OPTARG"
       ;;
   esac
 done
 
 export CLIENT_REPO=$GITHUB_WORKSPACE
-# export CLIENT_REPO=$(pwd)
-# echo "$(pwd)"
-# cd ../..
-# cd app/
-# echo "$(pwd)"
 cd /app
 echo "Start scan"
-python secretscan_entry.py --type "$SCAN_TYPE" --file "$FILE_EXCLUSIONS" --regex "$REGEX_EXCLUSIONS" --hash "$HASH_EXCLUSIONS" --cregex "$CUSTOM_REGEX" --code "$CLIENT_REPO"
+python action_entrypoint.py --type "$SCAN_TYPE" --code "$CLIENT_REPO"
 
