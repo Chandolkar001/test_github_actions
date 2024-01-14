2c5f03e21868922fb4cde321213b32e0650c0dd0
Chandolkar001 <chandolkarshreyas001@gmail.com>
2024-01-14 12:08:40 +0530

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,3 @@
-# #!/bin/sh -l
-
-# echo "Reading Parameters"
-
-# while getopts ":a:b:c:d:e:" opt; do
-#   case $opt in
-#     a)
-#       export SCAN_TYPE="$OPTARG"
-#       ;;
-#     b)
-#       export FILE_EXCLUSIONS="$OPTARG"
-#       ;;
-#     c)
-#       export REGEX_EXCLUSIONS="$OPTARG"
-#       ;;
-#     d)
-#       export HASH_EXCLUSIONS="$OPTARG"
-#       ;;
-#     e)
-#       export CUSTOM_REGEX="$OPTARG"
-#       ;;
-#   esac
-# done
-
-# export CLIENT_REPO=$GITHUB_WORKSPACE
-# cd /app
-# echo "Start scan"
-# python action_entrypoint.py --type "$SCAN_TYPE" --file "$FILE_EXCLUSIONS" --regex "$REGEX_EXCLUSIONS" --hash "$HASH_EXCLUSIONS" --cregex "$CUSTOM_REGEX" --code "$CLIENT_REPO"
-
 #!/bin/sh -l
 
 echo "Reading Parameters"
